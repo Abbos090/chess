91b287865e46cb5367bae26f9b9fdd9d50e3b5f4
Abbos <abboszafarxojaev@gmail.com>
2025-01-08 13:53:26 +0500

    shahmatdagi hamma yurishlar qo'shildi

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ def rook_check(x1,y1,x2,y2):
     return x1 == x2 or y1 == y2
 def bishop_check(x1,y1,x2,y2):
     return  abs(x1-x2) == abs(y1-y2)
+def king_check(x1,y1,x2,y2):
+    pass
+def knight_check(x1,y1,x2,y2):
+    return (x1-x2)**2 + (y1-y2)**2 == 5
+def minister(x1,y1,x2,y2):
+    return  abs(x1-x2) == abs(y1-y2) or x1 == x2 or y1 == y2
+def king_check(x1,y1,x2,y2):
+    return abs(x1-x2) == 1 or abs(y1-y2) == 1
